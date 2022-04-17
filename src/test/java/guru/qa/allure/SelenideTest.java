c5df2de21c432c2fb606a901934dd7a95599371c
Karina Sultanova <k.sultanova@dodopizza.com>
2022-04-18 01:01:19 +0600

    create SelenideTest

---
+++ src/test/java/guru/qa/allure/SelenideTest.java
@@ -0,0 +1,25 @@
+package guru.qa.allure;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTest {
+
+    @Test
+    void testGithubIssue(){
+        //km-sultanova/qa_guru_7_allure
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("76")).click();
+    }
+}
